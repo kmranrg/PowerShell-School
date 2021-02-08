21b0038624b1b3ea59678a01c634a61ab95d4dab
kmranrg <kmranrg@gmail.com>
2021-02-08 23:51:55 +0530

    hello world

---
+++ 01_HelloWorld/source.ps1
@@ -0,0 +1,28 @@
+<# 
+This is a simple Hello World Program in PowerShell.
+
+For running the code: `pwsh filename.ps1`
+#>
+
+Write-Host 'Hello, World!'
+
+# for printing the version of powershell
+$PSVersionTable
+
+<#
+OUTPUT:
+
+Hello, World!
+
+Name                           Value
+----                           -----
+PSVersion                      7.1.1
+PSEdition                      Core
+GitCommitId                    7.1.1
+OS                             Linux 5.8.0-41-generic #46~20.04.1-Ubuntu SMP Mon Jan 18 17:52:23 UTC 2021
+Platform                       Unix
+PSCompatibleVersions           {1.0, 2.0, 3.0, 4.0…}
+PSRemotingProtocolVersion      2.3
+SerializationVersion           1.1.0.1
+WSManStackVersion              3.0
+#>
