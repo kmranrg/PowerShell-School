6ddf8e140a2688712668cb3d914454fc93a6562d
kmranrg <kmranrg@gmail.com>
2021-02-19 00:11:25 +0530

    regular expressions complete with match quantifiers

--- 12_RegularExpressions/source.ps1
+++ 12_RegularExpressions/source.ps1
@@ -57,9 +57,9 @@ $res = "copy" -match "cpy$"
 Write-Host "->"$res
 
 Write-Host "8) Matches any instances (i.e. 0 or more instances) of the preceding expression (i.e. expression before the `*`)"
-$res = "mango" -match "g*"
+$res = "mango" -match "g*" # it will return True because the character 'g' is present 1 time
 Write-Host "->"$res
-$res = "mango" -match "z*"
+$res = "mango" -match "z*" # it will return True because the character 'z' is present 0 time
 Write-Host "->"$res
 
 Write-Host "9) Matches 1 or more instance(s) of the preceding expression"
@@ -74,14 +74,14 @@ Write-Host "->"$res
 $res = "mango" -match "(nz)+"
 Write-Host "->"$res
 
-Write-Host "10) Matches zero or one instance of the preceding expression"
+Write-Host "10) Matches zero or one instance of the preceding expression/character"
 $res = "mango" -match "g?"
 Write-Host "->"$res
 $res = "mano" -match "g?"
 Write-Host "->"$res
-$res = "magngo" -match "g?"
+$res = "magngo" -match "g?" # it will return True because it finds the character 'g' atleast one time
 Write-Host "->"$res
-$res = "maggngo" -match "g?"
+$res = "maggngo" -match "g?" # it will return True because it finds the character 'g' atleast one time
 Write-Host "->"$res
 
 # Match Character Classes
@@ -222,3 +222,190 @@ $res = "1223" -match "\D+" # it will return False because no non-digit character
 Write-Host "->"$res
 $res = "2@@" -match "\D+" # it will return True because it founds atleast one non-digit character
 Write-Host "->"$res
+
+# Match Quantifiers
+
+Write-Host "19)"
+<#
+Format:  *
+Logic: It matches 0 or more time(s) the preceding expression/character.
+       It is equivalent to {0,}.
+#>
+$res = "abc" -match "\w*"
+Write-Host "->"$res
+$res = "abc" -match "\w{0,}"
+Write-Host "->"$res
+$res = "" -match "\w*" # it will return True because it checks for zero or more matches and here it found zero match
+Write-Host "->"$res
+$res = "@" -match "\w" # it will return False because it does not find any character from (a-z,A-Z,_,0-9)
+Write-Host "->"$res
+
+Write-Host "20)"
+<#
+Format:  +
+Logic:   It matches 1 or more time(s) the preceding expression/character.
+#>
+$res = "xyxyxy" -match "xy+" # it will return True because "xy" is present
+Write-Host "->"$res
+$res = "xsy" -match "xy+" # it will return False because "xy" is not present even single time
+Write-Host "->"$res
+$res = "xyxsy" -match "xy+" # it will return True because "xy" is present atleast one time
+Write-Host "->"$res
+
+Write-Host "21)"
+<#
+Format:  ?
+Logic:   Specifies zero or one matches; for example, \w? or (abc)?.
+         It is equivalent to {0,1}.
+#>
+$res = "abcde" -match "\w?" # it will return True because it finds the character (i.e. a-z,A-Z,_,0-9) atleast 1 time
+Write-Host "->"$res
+$res = "abcde" -match "\w{0,1}" # it will return True because it finds the character (i.e. a-z,A-Z,_,0-9) atleast 1 time
+Write-Host "->"$res
+$res = "" -match "\w?" # it will return True because it finds the character (i.e. a-z,A-Z,_,0-9) atleast 0 time
+Write-Host "->"$res
+$res = "@" -match "\w?" # it will return True because it finds the character (i.e. a-z,A-Z,_,0-9) atleast 0 time
+Write-Host "->"$res
+
+Write-Host "22)"
+<#
+Format:  {n}
+Logic:   Checks for atleast n (i.e greater than or equal to n, where n >= 0) matches; for example, (pizza){2}.
+#>
+$res = "abc" -match "\w{2}"
+Write-Host "->"$res
+$res = "abc" -match "\w{4}" # it will return False because atleast 4 charcters from (a-z,A-Z,_,0-9) are not present
+Write-Host "->"$res
+$res = "abc" -match "\w{0}"
+Write-Host "->"$res
+$res = "" -match "\w{0}"
+Write-Host "->"$res
+$res = "pizzapizza" -match "(pizza){2}"
+Write-Host "->"$res
+$res = "pizzapizzsssa" -match "(pizza){2}" # it will return False because pizza is not present atleast 2 times
+Write-Host "->"$res
+
+Write-Host "23)"
+<#
+Format:  {n,m}
+Logic:   Checks the mentioned character/expression should be {atleast(>=) n} Logical-OR {atmost(<=) m} matches.
+#>
+$res = "abc" -match "\w{2,3}" # it will return True because total no of word characters are more than 2
+Write-Host "->"$res
+$res = "abcdefgh" -match "\w{2,5}" # it will return True because total no of word characters are more than 2
+Write-Host "->"$res
+$res = "a" -match "\w{2,5}" # it will return False because total no of word characters are less than 2
+Write-Host "->"$res
+$res = "afaafsafsfasf" -match "\w{,5}" # it will return False because total no of word characters are greater than 5 and it is expecting to have less than or equal to 5
+Write-Host "->"$res
+
+<#
+OUTPUT:
+
+1) Matching exact characters anywhere in the original value
+-> True
+-> True
+-> False
+2) Matches any single character
+-> True
+-> False
+-> False
+-> True
+3) Matches at least one of the characters in the brackets
+-> True
+-> False
+4) Matches at least one of the characters within the range. The use of a hyphen (-) allows you to specify an adjacent character.
+-> True
+-> False
+5) Matches any characters except those in brackets
+-> True
+-> False
+6) Matches the beginning characters
+-> True
+-> False
+-> True
+-> False
+-> False
+7) Matches the end characters
+-> True
+-> True
+-> False
+8) Matches any instances (i.e. 0 or more instances) of the preceding expression (i.e. expression before the *)
+-> True
+-> True
+9) Matches 1 or more instance(s) of the preceding expression
+-> True
+-> False
+-> True
+-> False
+-> False
+10) Matches zero or one instance of the preceding expression
+-> True
+-> True
+-> True
+-> True
+11)
+-> True
+-> False
+-> True
+-> False
+-> True
+-> False
+12)
+-> True
+-> False
+-> True
+13)
+-> True
+-> True
+-> True
+-> False
+14)
+-> True
+-> False
+-> False
+-> True
+-> False
+15)
+-> True
+-> False
+-> False
+16)
+-> True
+-> True
+-> False
+17)
+-> True
+-> True
+-> False
+18)
+-> True
+-> False
+-> True
+19)
+-> True
+-> True
+-> True
+-> False
+20)
+-> True
+-> False
+-> True
+21)
+-> True
+-> True
+-> True
+-> True
+22)
+-> True
+-> False
+-> True
+-> True
+-> True
+-> False
+23)
+-> True
+-> True
+-> False
+-> False
+#>
